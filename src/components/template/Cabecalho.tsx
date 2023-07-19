f464dc78f6494d95e642df5356ca449c680e9aa2
MessiasDevs <messiasvieira755@gmail.com>
2023-07-18 22:00:40 -0300

    feat: adição de componente cabecalho

--- src/components/template/Cabecalho.tsx
+++ src/components/template/Cabecalho.tsx
@@ -1,4 +1,6 @@
 
+import useAppData from '@/src/data/hook/useAppData'
+import BotaoAlternarTema from './BotaoAlternarTema'
 import Titulo from './Titulo'
 
 interface CabecalhoProps {
@@ -7,12 +9,16 @@ interface CabecalhoProps {
 }
 
 export default function Cabecalho(props: CabecalhoProps) {
+  const {tema, alternarTema } =useAppData()
   return (
-    <div>
+    <div className={`flex`}>
       <Titulo
       titulo={props.titulo}
       subtitulo={props.subtitulo}
       />
+      <div className={`flex flex-grow justify-end`}>
+        <BotaoAlternarTema tema={tema} alternarTema={alternarTema}/>
+      </div>
     </div>
   )
 }
